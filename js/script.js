7788c9b36717a51a9ea6b62fd9723ff5c63e8aba
Misha Sokol <sokol.m.m97@gmail.com>
2021-08-18 17:48:59 +0300

    V1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+let login = prompt ("Ведіть логін", '');
+if (login == "admin") {
+    let pasword = prompt ("Vedit, parol",'');
+    if (pasword == "admin") {
+        alert ("Privet brat");
+    }
+else if (login != login) {
+    alert ("vidmineno");
+}
+else {
+    alert ("i vas dont know");
+}
+}
---
+++ lesoon.js
@@ -0,0 +1,8 @@
+'use strict';
+let ansver = prompt ('Какое название Javascript&', '');
+
+if (ansver == 'ECMAScript') {
+    alert ("Zehr gur!");
+} else {
+alert ("Жмакай бібун");
+}
