7b3a47de562513eb504687265639e7c9597ac3a7
Misha Sokol <sokol.m.m97@gmail.com>
2021-08-18 18:47:26 +0300

    version 3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
 'use strict';
-let login = prompt ("Ведіть логін", '');
-if (login == "admin") {
-    let pasword = prompt ("Vedit, parol",'');
-    if (pasword == "admin") {
-        alert ("Privet brat");
-    }
-else if (login != login) {
-    alert ("vidmineno");
-}
-else {
-    alert ("i vas dont know");
-}
-}
+let data = 10;
+
+console.log (++data);
+console.log (++data);
+console.log (++data);
